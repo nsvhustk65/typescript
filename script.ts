3c49e5e06f1217b4cdee94b83270f46603c35f4c
nsvhustk65 <nsvhustk65@gmail.com>
2025-09-16 20:12:36 +0700

    first commit

---
+++ script.ts
@@ -0,0 +1,6 @@
+const chieudai =10;
+const chierong = 5;
+function chuvi(chieudai:number, chieurong:number):number{
+    return 2*(chieudai +chierong)
+};
+console.log(`Chu vi hình chữ nhật là: ${chuvi(chieudai, chierong)}`);
